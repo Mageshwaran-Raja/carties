4745db037c99bf2aa9bb2de89c0397c0d63bd34a
mageshwaran16 <mageshwaran.raja@kanini.com>
2023-10-15 16:07:57 +0530

    Auctions CRUD

---
+++ src/AuctionService/Data/AutionDbContext.cs
@@ -0,0 +1,14 @@
+using AuctionService.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace AuctionService.Data
+{
+    public class AutionDbContext : DbContext
+    {
+        public AutionDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<Auction> Auctions { get; set; }
+    }
+}
